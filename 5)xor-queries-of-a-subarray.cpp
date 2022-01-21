ca47b2841e9eed10f5f81dcfbf05adbe19618949
DhyeyRajveer <79468230+DhyeyRajveer@users.noreply.github.com>
2022-01-21 18:36:37 +0530

    Add files via upload

---
+++ 5)xor-queries-of-a-subarray.cpp
@@ -0,0 +1,38 @@
+/*
+Given an array and some ranges for that array,
+we hv to find the XOR of elements in those ranges
+Optimal solution:
+We create a vector(ref) which contains XOR till ith elements
+So ref.[i] = a[0]^a[1]^a[2]...a[i]
+For query [L,R], ans = ref[R] ^ ref[L-1]
+It works cuz ref[L-1] removes all elements b4 L cuz a^a = 0 & a^b^c = a^c^b..
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> refer{arr[0]};
+        vector<int> ans;
+        for(int i=1;i<arr.size();i++) refer.push_back(refer[i-1]^arr[i]);
+        for(auto x:queries)
+        {
+             int L = x[0], R = x[1];
+             if(L!=0) ans.push_back(refer[R]^refer[L-1]);
+             else ans.push_back(refer[R]);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+     Solution sol;
+     vector<int> arr{1,3,4,8};
+     vector<vector<int>> queries{{0,1},{1,2},{0,3},{3,3}};
+     vector<int> ans = sol.xorQueries(arr, queries);
+     for(int i=0;i<ans.size();i++) cout<<ans[i]<<" ";
+	return 0;
+}
---
+++ 6) All Pair Sum XOR.cpp
@@ -0,0 +1,31 @@
+/*
+Given: a array of integers(arr){4,3,9,1}
+We hv to find XOR of sum of each pair possible in arr
+So we hv to find (4+4)^(4+3)^(4,9)^(4,1) ^ (3,4)^(3,3)^.....
+It can be seen that each pair has a counter except (4,4) (3,3) (9,9) (1,1)
+For every pair like (4,3) there is (3,4)
+So they would cancel out and all we would need to do is find XOR of double of each element
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int allPairSumXOR(vector<int> arr)
+    {
+         int ans=0;
+         for(auto x:arr) ans ^= x*2;
+         return ans;
+    }
+};
+
+int main()
+{
+     Solution sol;
+     vector<int> arr{2,4,1};
+     vector<vector<int>> arr2{{0,1},{1,2},{0,3},{3,3}};
+     auto ans = sol.allPairSumXOR(arr);
+     cout<<ans<<" ";
+     return 0;
+}
